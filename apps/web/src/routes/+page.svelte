3433e85dccd829d015d4cc8d35298609e93c9d28
Samuel Andert <samuel@ora.earth>
2023-09-25 19:56:08 +0200

    deploy test fix

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -5,10 +5,7 @@
 	export let data: PageData;
 </script>
 
-<h1>Web</h1>
 <MyCounterButton />
-
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 {#each data.countries_countries ?? [] as country}
 	<div>{country.name}</div>
